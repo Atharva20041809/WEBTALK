6b5ce52ed662ed42e551eced97728c60ba6d8ba6
Atharva Tiwari <atharvatiwari@atharvas-MacBook-Pro-2.local>
2025-11-05 20:11:27 +0530

    another commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Register from './pages/Register.jsx';
 import Login from './pages/Login.jsx';
 import Home from './pages/Home.jsx';
+
 function App() {
   return (
     <Routes>
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API =  axios.create({
-    baseURL: 'https://webtalk-backend-r1xs.onrender.com'
+    baseURL: 'https://webtalk-8ank.onrender.com'
     // baseURL: 'http://localhost:5000'
 })
 export function signupUser(data){
